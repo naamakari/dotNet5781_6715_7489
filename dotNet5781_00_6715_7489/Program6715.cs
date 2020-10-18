e5d9ad3d38ab67a659d2edf00a0631f395dac446
רננה כהן <rencohen@g.jct.ac.il>
2020-10-18 21:52:46 +0300

    הוספנו פונקציה בפעם השנייה:)

---
+++ dotNet5781_00_6715_7489/Program6715.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6715_7489
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome6715();
+            welcome7489();
+            Console.ReadKey();
+        }
+
+        static partial void welcome7489();
+       
+        private static void welcome6715()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
