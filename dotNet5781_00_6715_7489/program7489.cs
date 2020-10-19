27a295c34816c20920303db0434a39fd8bfc9642
naama kari <naamakari@gmail.com>
2020-10-19 21:20:10 +0300

    checking:)

---
+++ dotNet5781_00_6715_7489/program7489.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_6715_7489
+{
+   partial class Program
+    {
+        static partial void welcome7489()
+        {
+            Console.WriteLine("hello renana:)");
+        }
+    }
+}
