62f86b8a9517d1be985aabdb23d7b238a4f2b8f8
רננה כהן <rencohen@g.jct.ac.il>
2020-11-05 02:22:32 +0200

    שטויות

--- dotNet5781_02_6715_7489/Program.cs
+++ dotNet5781_02_6715_7489/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -13,9 +14,19 @@ namespace dotNet5781_02_6715_7489
  
         static void Main(string[] args)
         {
-            //busStation bs=new busStation("1234","naharia");
-            //bs.ToString();
-            //Console.WriteLine(bs);
+            List<int> myList = new List<int>{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach(int item in myList)
+                Console.WriteLine(item);
+            int firstIndex, lastIndex;
+            //מוצאים את האינדקס ברשימה לפני הקוד הראשון שניתן
+            firstIndex = myList.FindIndex(x => x==2);
+            //מוצאים את האינדקס ברשימה לפי הקוד השני שניתן
+            lastIndex = myList.FindIndex(x => x==7);
+            //אם אחד מקודי התחנה לא נמצאים
+            
+            //תוסיף לסכום את כל מרחקי הנסיעה של אחת מהתחנות
+            for (int i = firstIndex; i <= lastIndex; i++)
+                Console.WriteLine(myList[i]);
             Console.ReadKey();
         }
     }
