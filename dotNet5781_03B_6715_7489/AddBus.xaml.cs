36a110776de30a60bdef2242fe07a9addbf37b1b
naama kari <naamakari@gmail.com>
2020-11-24 00:37:48 +0200

    עוד תוספות מעניינות

--- dotNet5781_03B_6715_7489/AddBus.xaml.cs
+++ dotNet5781_03B_6715_7489/AddBus.xaml.cs
@@ -22,28 +22,113 @@ namespace dotNet5781_03B_6715_7489
         public AddBus()
         {
             InitializeComponent();
-        } 
-        
+        }
+        static int counter = 0;
+        private bool nonNumeriable=false;
         private void tbLiNum_KeyDown(object sender, KeyEventArgs e)
         {
+            nonNumeriable = false;
+            counter++;
+            //if the key is not a number from the up keyboard
+            if (e.Key < Key.D0 || e.Key > Key.D9)
+                //if the key is not a number from the side keyboard
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                        nonNumeriable = true;
 
+            if (counter == 8 || nonNumeriable == true)
+            {
+                e.Handled = true;
+                counter = 0;
+            }
         }
-
-        private void tbTreat_KeyDown(object sender, TextChangedEventArgs e)
+        private void tbTreat_KeyDown(object sender, KeyEventArgs e)
         {
+            nonNumeriable = false;
+            counter++;
+            //if the key is not a number from the up keyboard
+            if (e.Key < Key.D0 || e.Key > Key.D9)
+                //if the key is not a number from the side keyboard
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                    if (e.Key != Key.Decimal)
+                        nonNumeriable = true;
 
+            if (counter == 8 || nonNumeriable == true)
+            {
+                e.Handled = true;
+                counter = 0;
+            }
         }
-
         private void tbRef_KeyDown(object sender, KeyEventArgs e)
         {
+            nonNumeriable = false;
+            counter++;
+            //if the key is not a number from the up keyboard
+            if (e.Key < Key.D0 || e.Key > Key.D9)
+                //if the key is not a number from the side keyboard
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                    if (e.Key != Key.Decimal)
+                        nonNumeriable = true;
 
+            if (counter == 8 || nonNumeriable == true)
+            {
+                e.Handled = true;
+                counter = 0;
+            }
         }
 
         private void tbKm_KeyDown(object sender, KeyEventArgs e)
         {
+            nonNumeriable = false;
+            counter++;
+            //if the key is not a number from the up keyboard
+            if (e.Key < Key.D0 || e.Key > Key.D9)
+                //if the key is not a number from the side keyboard
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                    if (e.Key != Key.Decimal)
+                        nonNumeriable = true;
 
+            if (counter == 8 || nonNumeriable == true)
+            {
+                e.Handled = true;
+                counter = 0;
+            }
         }
 
-      
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tbKm_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbTreat.Text != "" && tbRef.Text != "" && tbLiNum.Text != "" && tbKm.Text != "")
+                add.IsEnabled = true;
+            else
+                add.IsEnabled = false;
+        }
+
+        private void tbLiNum_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (dateSt.SelectedDate!=null && dateTreat.SelectedDate != null && tbTreat.Text != "" && tbRef.Text != "" && tbLiNum.Text != "" && tbKm.Text != "")
+                add.IsEnabled = true;
+            else
+                add.IsEnabled = false;
+        }
+
+        private void tbTreat_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (dateSt.Text!=null&&dateTreat.Text!=null&& tbTreat.Text != "" && tbRef.Text != "" && tbLiNum.Text != "" && tbKm.Text != "")
+                add.IsEnabled = true;
+            else
+                add.IsEnabled = false;
+        }
+
+        private void tbRef_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (tbTreat.Text != "" && tbRef.Text != "" && tbLiNum.Text != "" && tbKm.Text != "")
+                add.IsEnabled = true;
+            else
+                add.IsEnabled = false;
+        }
     }
 }
