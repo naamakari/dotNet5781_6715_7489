e64d09c7fd23111ff4c556522730daa8179f06ea
רננה כהן <rencohen@g.jct.ac.il>
2020-11-30 20:15:21 +0200

    ישששש

--- dotNet5781_03B_6715_7489/busStatic.cs
+++ dotNet5781_03B_6715_7489/busStatic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,11 @@ namespace dotNet5781_03B_6715_7489
 {
     static public class busStatic
     {
-       static public List<Bus> buses;
+        
+       static public ObservableCollection<Bus> buses;
         static busStatic()
         {
-            buses = new List<Bus>();
+            buses = new ObservableCollection<Bus>();
             //the treat time of the bus is passed alreay
             buses.Add(new Bus("7297962", new DateTime(2012, 4, 25), new DateTime(2015, 11, 25), 8002, 520, 205000));
             //this buses is close to the kilometraz
