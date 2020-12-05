64d091012dcb7c5a3bce5bf8b668bbd7f82ba359
רננה כהן <rencohen@g.jct.ac.il>
2020-12-05 19:52:45 +0200

    ניסיתי לעשות תהליכון לנסיעה ושום מה הוא לא משתהה מספיק

--- dotNet5781_03B_6715_7489/toDrive.xaml.cs
+++ dotNet5781_03B_6715_7489/toDrive.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Threading;
+using System.ComponentModel;
 using dotNet5781_01_6715_7489;
 
 namespace dotNet5781_03B_6715_7489
@@ -20,12 +22,15 @@ namespace dotNet5781_03B_6715_7489
     /// </summary>
     public partial class toDrive : Window
     {
+
         public toDrive()
         {
             InitializeComponent();
         }
+        BackgroundWorker DriveWorker;
         public Bus myBus { get; set; }//definaition of proparthy of bus we selected for the new window we opened
         private bool nonNumeriable = false;
+        static public Random rand = new Random(DateTime.Now.Millisecond);
         private void dis_KeyDown(object sender, KeyEventArgs e)//An event of inserting keys from the keyboard
         {
             nonNumeriable = false;
@@ -45,10 +50,7 @@ namespace dotNet5781_03B_6715_7489
                 {
                     if (diff.TotalDays < 365 && myBus.kmSinceLastTreat + float.Parse(dis.Text) <= 20000)//can take the driving from the treat aspect
                     {
-                        myBus.upDateDetails(double.Parse(dis.Text));//update the details of the bus according the km
-                        myBus.stateBus = state.inDrive;//change the status of the bus
-                        //שולח לנסיעה
-                        //לעדכן את הזמן, להוציא הודעה מתאימה ולעדכן את הסטטוס
+                        this.driving();
                     }
 
                     else//the bus need treat and can not take the driving
@@ -87,6 +89,37 @@ namespace dotNet5781_03B_6715_7489
         {
             dis.Text = "";
 
+        }
+
+        private void driving()
+        {
+            DriveWorker = new BackgroundWorker();
+            DriveWorker.DoWork += DriveWorker_DoWork;
+            DriveWorker.RunWorkerCompleted += DriveWorker_RunWorkerCompleted;//Event registration
+
+            DriveWorker.RunWorkerAsync(myBus);//start the process
+            myBus.upDateDetails(double.Parse(dis.Text));//update the details of the bus according the km
+            myBus.stateBus = state.inDrive;//change the status of the bus
+
+
+                                           //שולח לנסיעה
+                                           //לעדכן את הזמן, להוציא הודעה מתאימה ולעדכן את הסטטוס
+        }
+
+        private void DriveWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            float kmForH= rand.Next(20, 50);
+            int time =(int)(float.Parse(dis.Text)/kmForH);
+            Thread.Sleep(time*60000);//drive is done accord the km and the bus speed 
+            //לא משתהה מספיק לא יודעת למה
+        }
+
+        private void DriveWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //currentBus = (Bus)e.Result;//castong for bus 
+            myBus.stateBus = state.ready;//use in Bus external for changed in the bus during the process 
+            string numLine = myBus.Id;
+            MessageBox.Show(" אוטובוס מספר " + numLine + " חזר מנסיעה", "סיום הנסיעה");
         }
     }
 }
