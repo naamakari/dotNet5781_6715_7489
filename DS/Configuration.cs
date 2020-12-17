7f163e0da45fa8555bd1dd422a8180022af7be12
naama kari <naamakari@gmail.com>
2020-12-17 14:30:59 +0200

    vjvjnvknk

--- DS/Configuration.cs
+++ DS/Configuration.cs
@@ -17,6 +17,7 @@ namespace DS
         public static int GetBusLineRunNum()
         {
             return ++BusLineRunNum;
+            
         }
         public static int GetBusStationRunNum()
         {
