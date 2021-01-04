17920ccf7b5dd7ff9cba17ceaf96ffa886c988de
רננה כהן <rencohen@g.jct.ac.il>
2021-01-05 01:35:27 +0200

    כעי

--- UIWpf/BusStationsWindow.xaml.cs
+++ UIWpf/BusStationsWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace UIWpf
     /// </summary>
     public partial class BusStationsWindow : Window
     {
+        private bool nonNumeriable = false;
         IBL bl;
         ObservableCollection<BusStationBL> stations=new ObservableCollection<BusStationBL>();
         ObservableCollection<BusLine> busLines=new ObservableCollection<BusLine>();
@@ -48,12 +49,17 @@ namespace UIWpf
 
         private void DeleteBusLine_Click(object sender, RoutedEventArgs e)
         {
-
+            BusStationBL busStation = busStationBLDataGrid.SelectedItem as BusStationBL;
+            stations.Remove(busStation);
+            busStationDetailes.DataContext = null;
+            collectionBusLinesListView.ItemsSource = null;
         }
 
-        private void addBusLine_Click(object sender, RoutedEventArgs e)
+        private void addBusStation_Click(object sender, RoutedEventArgs e)
         {
-
+            busStationDetailes.Visibility = Visibility.Hidden;
+            addBusStationGrid.Visibility = Visibility.Visible;
+            addStation.IsEnabled = false;
         }
 
         private void backToNenu_Click(object sender, RoutedEventArgs e)
@@ -69,6 +75,88 @@ namespace UIWpf
         private void busStationBLDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             
+            DeleteStation.IsEnabled = true;
+            UpdateStation.IsEnabled = true;
+            BusStationBL busStation = busStationBLDataGrid.SelectedItem as BusStationBL;
+            busStationDetailes.DataContext = busStation;
+            if (busStation != null)
+                collectionBusLinesListView.ItemsSource = busStation.CollectionBusLines;
+            
         }
+
+        private void latitudeTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            nonNumeriable = false;
+
+            //if the key is not a number from the up keyboard
+            if (e.Key < Key.D0 || e.Key > Key.D9)
+                //if the key is not a number from the side keyboard
+                if (e.Key < Key.NumPad0 || e.Key > Key.NumPad9)
+                    if (e.Key != Key.Decimal)
+                        nonNumeriable = true;
+
+            if (nonNumeriable == true)
+            {
+                e.Handled = true;
+
+            }
+        }
+
+        private void stationNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            enable();
+        }
+
+        private void addressTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            enable();
+        }
+
+        private void latitudeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            enable();
+        }
+
+        private void longitudeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            enable();
+        }
+        private void enable()
+        {
+            if (stationNameTextBox.Text != "" && addressTextBox.Text != "" && latitudeTextBox.Text != "" &&
+                longitudeTextBox.Text != "")
+                toAdd.IsEnabled = true;
+        }
+
+        private void toAdd_Click(object sender, RoutedEventArgs e)
+        {
+            BusStationBL busStation = new BusStationBL()
+            {
+                StationName = stationNameTextBox.Text,
+                Address = addressTextBox.Text,
+                Latitude = float.Parse(latitudeTextBox.Text),
+                Longitude = float.Parse(longitudeTextBox.Text),
+            };
+            busStation.Location= busStation.Latitude + "°N " + busStation.Longitude + "°E";
+            try 
+            {
+                bl.AddBusStation(busStation);
+                foreach (BusStationBL item in bl.GetAllStations())
+                    stations.Add(item);
+                busStationDetailes.DataContext = null;
+                busStationDetailes.Visibility = Visibility.Visible;
+                addBusStationGrid.Visibility = Visibility.Hidden;
+                MessageBox.Show("התחנה נוספה בהצלחה", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                addStation.IsEnabled = true;
+
+            }
+               
+            catch (BO.DalAlreayExistExeption ex)
+            {
+                MessageBox.Show(ex.Message, "הודעת מערכת", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            
+            
+            }
     }
 }
