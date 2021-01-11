7219b537071f88d79d472d4c06e4879748e7af7d
רננה כהן <rencohen@g.jct.ac.il>
2021-01-12 01:34:10 +0200

    123

--- UIWpf/LinePath.xaml.cs
+++ UIWpf/LinePath.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 using BL;
 using BO;
@@ -29,9 +30,10 @@ namespace UIWpf
     {
 
         private Stopwatch stopWatch;
-        private bool isTimerRun=false;
+        private bool isTimerRun = false;
         BackgroundWorker timerworker;
         string str;
+        IEnumerable<BusStationBL> stations;
         IEnumerable<BusLineBL> busLineBLsPossiblePath;
         IBL bl;
 
@@ -39,10 +41,11 @@ namespace UIWpf
         {
             bl = _bl;
             InitializeComponent();
+            stations = bl.GetAllStations();
 
-        
-            firstStationComboBox.ItemsSource = bl.GetAllStations();
-            lastStationComboBox.ItemsSource = bl.GetAllStations();
+
+            firstStationComboBox.ItemsSource = stations;
+            lastStationComboBox.ItemsSource = stations;
 
 
             // stopWatch = new Stopwatch();
@@ -51,12 +54,12 @@ namespace UIWpf
             timerworker.DoWork += Timerworker_DoWork;
             timerworker.ProgressChanged += Timerworker_ProgressChanged;
             timerworker.WorkerReportsProgress = true;
-            float num = (float)130.5;
+
             // MessageBox.Show( TimeSpan.FromMinutes(num).ToString());
             //  timeSpan = TimeSpan.Parse(num.ToString());
             // MessageBox.Show(timeSpan.ToString());
             //MessageBox.Show(DateTime.Now.Hour+DateTime.Now.Minute.ToString()+DateTime.Now.Second);
-            
+
 
         }
 
@@ -71,19 +74,26 @@ namespace UIWpf
 
         private void Timerworker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
+
             BusStation currBusStation = firstStationComboBox.SelectedItem as BusStation;
             str = DateTime.Now.ToString();
             timer.Text = str.Substring(10, 9);
-            //string timerText = stopWatch.Elapsed.ToString();
-            // timerText = timerText.Substring(0, 8);
-            //this.timerTextBlock.Text = timerText;
-            lineTimingDataGrid.DataContext = bl.GetLineTimingsAccordingLine(busLineBLsPossiblePath, currBusStation);
+            IEnumerable<LineTiming> lineTimings = bl.GetLineTimingsAccordingLine(busLineBLsPossiblePath, currBusStation);
+
+            lineTimingDataGrid.DataContext = lineTimings;
+            if (lineTimings.Count() == 0)
+                exeption.Visibility = Visibility.Visible;
+
+
         }
 
 
         private void LookAtTimeOfLines_Click(object sender, RoutedEventArgs e)
         {
-         
+            lineTimingDataGrid.Visibility = Visibility.Visible;
+            timer.Visibility = Visibility.Visible;
+            StopWatch.IsEnabled = true;
+
             if (!isTimerRun)
             {
                 // stopWatch.Restart();
@@ -103,36 +113,69 @@ namespace UIWpf
                 timer.Visibility = Visibility.Hidden;
                 isTimerRun = false;
             }
+            exeption.Visibility = Visibility.Hidden;
+            LookAtTimeOfLines.IsEnabled = false;
+            searchLines.IsEnabled = false;
+            StopWatch.IsEnabled = false;
+            search1.Text = "";
+            search2.Text = "";
+            firstStationComboBox.SelectedItem = null;
+            lastStationComboBox.SelectedItem = null;
         }
 
         private void searchLines_Click(object sender, RoutedEventArgs e)
         {
             BusStation firstStation = firstStationComboBox.SelectedItem as BusStation;
             BusStation lastStation = lastStationComboBox.SelectedItem as BusStation;
-          //  string first = firstStationComboBox.SelectedItem as string;
+            LookAtTimeOfLines.IsEnabled = true;
+            searchLines.IsEnabled = true;
+            //  string first = firstStationComboBox.SelectedItem as string;
 
             try
             {
-                busLineBLsPossiblePath= bl.GetPossiblePath(firstStation.StationCode, lastStation.StationCode);
+                busLineBLsPossiblePath = bl.GetPossiblePath(firstStation.StationCode, lastStation.StationCode);
                 busLineBLDataGrid.ItemsSource = busLineBLsPossiblePath;
 
             }
             catch (BO.DalEmptyCollectionExeption)
             {
-               // busLineBLDataGrid.ItemsSource = null;
+                ;
             }
         }
 
         private void firstStationComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+
             if (firstStationComboBox.SelectedItem != null && lastStationComboBox.SelectedItem != null && (firstStationComboBox.SelectedItem.ToString() != lastStationComboBox.SelectedItem.ToString()))
                 searchLines.IsEnabled = true;
-            else if (firstStationComboBox.SelectedItem != null&& lastStationComboBox.SelectedItem != null&&firstStationComboBox.SelectedItem.ToString() == lastStationComboBox.SelectedItem.ToString())
+            else if (firstStationComboBox.SelectedItem != null && lastStationComboBox.SelectedItem != null && firstStationComboBox.SelectedItem.ToString() == lastStationComboBox.SelectedItem.ToString())
             {
                 MessageBox.Show("תחנת המוצא והיעד לא אפשריות, בחר/י תחנות מוצא ויעד אחרות", "הודעת מערכת", MessageBoxButton.OK, MessageBoxImage.Exclamation);
                 firstStationComboBox.SelectedItem = null;
                 lastStationComboBox.SelectedItem = null;
             }
+        }
+
+        private void search1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (search1.Text != "")
+            {
+                IEnumerable<BusStationBL> busStationCollection = stations.Where(x => x.StationName.Contains(search1.Text));
+                firstStationComboBox.ItemsSource = busStationCollection;
+            }
+            else
+                firstStationComboBox.ItemsSource = bl.GetAllStations();
+        }
+
+        private void search2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (search2.Text != "")
+            {
+                IEnumerable<BusStationBL> busStationCollection = stations.Where(x => x.StationName.Contains(search2.Text));
+                lastStationComboBox.ItemsSource = busStationCollection;
+            }
+            else
+                lastStationComboBox.ItemsSource = bl.GetAllStations();
         }
     }
 }
