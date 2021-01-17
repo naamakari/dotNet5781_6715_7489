69240bdd73e48705d7d28e24972f0c9dc54d40fc
רננה כהן <rencohen@g.jct.ac.il>
2021-01-17 18:53:23 +0200

    12

--- APIDAL/IDAL.cs
+++ APIDAL/IDAL.cs
@@ -78,6 +78,8 @@ namespace APIDAL
 
         LineTrip GetLineTripBy(Predicate<LineTrip> predicate);
         void AddLineTrip(LineTrip lineTrip);
+       IEnumerable<LineTrip> getLineTripBy(Predicate<LineTrip> condition);
+        void deleteLineTrip(LineTrip lineTrip);
 
         
     }
